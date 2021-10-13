243f8b4804375a38d4aa914f1739d9715011584e
June Hong <junehong69@gmail.com>
2021-10-12 19:36:23 -0700

    Remove test code

--- ConsoleDisplay.cpp
+++ ConsoleDisplay.cpp
@@ -6,19 +6,16 @@ using namespace std;
 
 void ConsoleDisplay::printNewGameMessage()
 {
-// #if (TARGET_HW == LINUX_VM)
 #ifdef LINUX_VM
 	cout << "New game" << endl;
 #endif
 
 #ifdef EVM_1_0
 	// To do ...
-	cout << "HW10 - New game" << endl;
 #endif
 
 #ifdef EVM_1_1
 	// To do ...
-	cout << "HW11 - New game" << endl;
 #endif
 
 }
@@ -40,36 +37,14 @@ void ConsoleDisplay::promptUserToGuess(char* user_guess)
 
 #ifdef EVM_1_0
 	// To do ...
-	cout << "HW10 - Take a guess: ";
-	for (int i = 0; i < num_of_buttons; i++) {
-		do {
-			cin >> user_guess[i];
-		} while (!inputCorrect(toupper(user_guess[i])));
-	}
-	// Make all letters uppercase
-	for (int i = 0; i < num_of_buttons; i++) {
-		user_guess[i] = toupper(user_guess[i]);
-	}
 #endif
 
 #ifdef EVM_1_1
 	// To do ...
-	cout << "HW11 - Take a guess: ";
-	for (int i = 0; i < num_of_buttons; i++) {
-		do {
-			cin >> user_guess[i];
-		} while (!inputCorrect(toupper(user_guess[i])));
-	}
-	// Make all letters uppercase
-	for (int i = 0; i < num_of_buttons; i++) {
-		user_guess[i] = toupper(user_guess[i]);
-	}
 #endif
 }
 
 #ifdef LINUX_VM
-// #ifdef EVM_1_0
-// #ifdef EVM_1_1
 bool ConsoleDisplay::inputCorrect(char guess)
 {
 	bool retval = false;
@@ -97,18 +72,10 @@ void ConsoleDisplay::printResult(int* led_colors)
 
 #ifdef EVM_1_0
 	// To do ...
-	for (int i = 0; i < num_of_buttons; i++) {
-		cout << "HW10 - LED" << i+1 << ":" << led_colors_str[led_colors[i]] << " ";
-	}
-	cout << endl;
 #endif
 
 #ifdef EVM_1_1
 	// To do ...
-	for (int i = 0; i < num_of_buttons; i++) {
-		cout << "HW11 - LED" << i+1 << ":" << led_colors_str[led_colors[i]] << " ";
-	}
-	cout << endl;
 #endif
 }
 
@@ -120,11 +87,9 @@ void ConsoleDisplay::printWinMessage()
 
 #ifdef EVM_1_0
 	// To do ...
-	cout << "HW10 - YOU WIN!!! " << endl;
 #endif
 
 #ifdef EVM_1_1
 	// To do ...
-	cout << "HW11 - YOU WIN!!! " << endl;
 #endif
 }
--- README - How to migrate to new target hardware..md
+++ README - How to migrate to new target hardware..md
@@ -10,28 +10,25 @@
 
 1) Define target hardware name and set compile option in __makefile__.
 ```
-PROG = ThreeButtons
-CC = g++
-CFLAGS = -Wall -g -std=c++11
 # For Linux Console
-#COMPILE_OPTS = -DTARGET_HW=LINUX_VM
-# For EVM Board version 1.0   
-COMPILE_OPTS = -DTARGET_HW=EVM_1_0  # <=== Set compile option here. ex) EVM_1_0
+# COMPILE_OPTS = -DLINUX_VM
+# For EVM Board version 1.0
+# COMPILE_OPTS = -DEVM_1_0		# <=== Set target hardware here. ex) EVM_1_0
 # For EVM Board version 1.1
-# COMPILE_OPTS = -DTARGET_HW=EVM_1_1
-LDFLAGS =
-OBJS = ThreeButtons.o Game.o ConsoleDisplay.o
+# COMPILE_OPTS = -DEVM_1_1
 ```
 
 2) Implement target hardware functions in __ConsoleDisplay.cpp__ file.
 ```
-#if (TARGET_HW == LINUX_VM)
+#ifdef LINUX_VM
 	cout << "New game" << endl;
-#elif (TARGET_HW == EVM_1_0)
-	// To do ...                 # <=== Implement target hardware function here.
-#elif (TARGET_HW == EVM_1_1)
+#endif
+
+#ifdef EVM_1_0
+	// To do ...					# <=== Implement target hardware function here.
+#endif
+
+#ifdef EVM_1_1
 	// To do ...
-#else
-	#error No target selected
 #endif
 ```
